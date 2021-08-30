3d4f11ef0b85b5cef5a442ed9cd2280e743e2f81
zura-japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-08-30 18:49:08 +0400

    up

--- js/L_script.js
+++ js/L_script.js
@@ -29,3 +29,22 @@ ag_arrow.addEventListener("click", function () {
   ag_arrow.classList.toggle("active-arrow");
   ag_list.classList.toggle("dn");
 });
+
+
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+  arrows: false
+});
