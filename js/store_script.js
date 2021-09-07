6a8ef00682772b9bce9e1847c97bcad90a9c3443
zura-japoshvili <73229102+zura-japoshvili@users.noreply.github.com>
2021-09-07 21:09:24 +0400

    update

--- js/store_script.js
+++ js/store_script.js
@@ -29,3 +29,43 @@ active_burger.addEventListener("click", function(){
   side_nav.classList.remove("side-nav-active");
   burger_icon.style.visibility = "visible";
 });
+
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: false
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
